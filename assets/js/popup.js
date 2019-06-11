2f4f61a5d4f629907c5eef98e2721b9209182a87
almasantang <almasantang@gmail.com>
2019-06-12 03:00:14 +1000

    update check yourself

---
+++ assets/js/popup.js
@@ -0,0 +1,138 @@
+
+// $("#close-window1").click(function() {
+// 		$(".pop-up1").removeClass('pop-up1');
+// });
+
+// $("#close-window2").click(function() {
+// 		$(".pop-up2").fadeOut(200);
+// });
+
+// $("#close-window3").click(function() {
+// 		$(".pop-up3").fadeOut(200);
+// });
+
+// $(document).ready(function(){
+//   $(".pop-up1").on("click", function(){
+//     $(this).hide();
+//   });
+// });
+
+$(document).ready(function(){
+  $(".close-button-1").on("click", function(){
+    $(".pop-up1").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-2").on("click", function(){
+    $(".pop-up2").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-3").on("click", function(){
+    $(".pop-up3").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-4").on("click", function(){
+    $(".pop-up4").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-5").on("click", function(){
+    $(".pop-up5").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-6").on("click", function(){
+    $(".pop-up6").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-7").on("click", function(){
+    $(".pop-up7").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-8").on("click", function(){
+    $(".pop-up8").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-9").on("click", function(){
+    $(".pop-up9").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-10").on("click", function(){
+    $(".pop-up10").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-11").on("click", function(){
+    $(".pop-up11").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-12").on("click", function(){
+    $(".pop-up12").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-13").on("click", function(){
+    $(".pop-up13").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-14").on("click", function(){
+    $(".pop-up14").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-15").on("click", function(){
+    $(".pop-up15").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-16").on("click", function(){
+    $(".pop-up16").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-17").on("click", function(){
+    $(".pop-up17").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-18").on("click", function(){
+    $(".pop-up18").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-19").on("click", function(){
+    $(".pop-up19").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".close-button-20").on("click", function(){
+    $(".pop-up20").hide();
+  });
+});
